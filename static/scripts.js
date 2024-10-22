4ddce4c00e26f758584748f0acec3d461eebe3dd
wkobiela <wuch.mail@gmail.com>
2024-10-22 20:39:48 +0200

    Add some polish translations

--- static/scripts.js
+++ static/scripts.js
@@ -13,6 +13,31 @@ $(document).ready(function() {
 
     $('#produktyTable').DataTable({
         autoWidth: true, 
+        language: {
+            decimal: ",",
+            emptyTable: "Brak danych",
+            info: "Pokazuje _START_ do _END_ z _TOTAL_ wpisów",
+            infoEmpty: "Pokazuje 0 do 0 z 0 wpisów",
+            infoFiltered: "(filtrowane z _MAX_ całkowitych wpisów)",
+            infoPostFix: "",
+            thousands: ".",
+            lengthMenu: "Pokaż _MENU_ wpisów",
+            loadingRecords: "Ładowanie...",
+            processing: "Przetwarzanie...",
+            search: "Szukaj:",
+            searchPlaceholder: "Szukaj...",
+            zeroRecords: "Nie znaleziono żadnych pasujących wpisów",
+            paginate: {
+                first: "Pierwsza",
+                last: "Ostatnia",
+                next: "Następna",
+                previous: "Poprzednia"
+            },
+            aria: {
+                sortAscending: ": aktywuj, aby sortować kolumnę rosnąco",
+                sortDescending: ": aktywuj, aby sortować kolumnę malejąco"
+            }
+        },
         columns: [
             { data: 'caliber' },
             { data: 'shop' },
